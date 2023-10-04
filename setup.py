4a4e491fe39e9180185b9cb47bd6b1bdfa9f4ef8
Campeone <Ojotimilehin01@gmail.com>
2023-10-04 23:48:08 +0100

    Project template created with cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An end to tnd Machine learning project that detect the emotion behind a movie review text. The project is ultimately deployed online to be made available to the end user.',
+    author='Campeone',
+    license='MIT',
+)
